aef44b61e4d6bd38d3aea8d44c07e50d901ea5f3
Augusto <augusto.raminelli>
2022-05-26 20:46:33 -0300

    feat: rendering list users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { gql, useQuery } from "@apollo/client";
+import { User } from "./types/User";
+
+const GET_USERS = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+  `;
 
 function App() {
- 
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USERS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ul>
+      {data && data.users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   )
 }
 
