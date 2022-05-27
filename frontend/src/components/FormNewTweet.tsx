d085cc87f4228c4bdaa95ae01bc34b113be0f883
Augusto <augusto.raminelli>
2022-05-27 17:57:48 -0300

    feat: created mutation createTweet

--- frontend/src/components/FormNewTweet.tsx
+++ frontend/src/components/FormNewTweet.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, FormEvent } from 'react';
 import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
@@ -8,7 +8,35 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
+import { gql, useMutation } from "@apollo/client";
+import { GET_TWEETS } from '../pages/Home';
+
+const CREATE_TWEET = gql`
+mutation ($tweet: String!) {
+  createTweet(tweet: $tweet) {
+    id
+    tweet
+  }
+}
+`;
+
 export function FormNewTweet() {
+  const [tweet, setTweet] = useState("");
+  const [createTweet, { data, loading, error }] = useMutation(CREATE_TWEET);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!tweet) {
+      return;
+    }
+    await createTweet({
+      variables: { tweet },
+      refetchQueries: [GET_TWEETS]
+    });
+    setTweet("");
+  }
+
   return (
     <Box sx={{ width: '90%', maxWidth: 500, marginBottom: "2em" }}>
       <h1>Home</h1>
@@ -18,6 +46,7 @@ export function FormNewTweet() {
         </InputLabel>
         <Input
           id="input-with-icon-adornment"
+          onChange={(e) => setTweet(e.target.value)}
           startAdornment={
             <InputAdornment position="start">
               <AccountCircle />
@@ -25,7 +54,11 @@ export function FormNewTweet() {
           }
         />
       </FormControl>
-      <Button variant="contained" endIcon={<SendIcon />}>
+      <Button
+        endIcon={<SendIcon />}
+        onClick={handleSubmit}
+        variant="contained"
+      >
         Meliweet
       </Button>
     </Box>
