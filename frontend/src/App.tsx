951ae77e314a6927493b0ff743ed590eb08eaf78
Augusto <augusto.raminelli>
2022-05-26 21:39:17 -0300

    feat: created mutation delete User

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import { NewUserForm } from "./component/NewUserForm";
 import { User } from "./types/User";
 
@@ -11,11 +11,36 @@ export const GET_USERS = gql`
   }
   `;
 
+  const DELETE_USER = gql`
+
+  mutation ($id: ID!) {
+    deleteUser(id: $id) {
+      id
+      name
+    }
+  }
+  `;
+
 function App() {
   const { data, loading } = useQuery<{ users: User[] }>(GET_USERS);
+  const [deleteUser] = useMutation(DELETE_USER);
 
   if (loading) {
     return <p>Loading...</p>;
+  }
+
+  const handleDelete = async (id: string) => {
+    
+    if (!id) {
+      return;
+    }
+
+    await deleteUser({
+      variables: { id },
+      refetchQueries: [GET_USERS]
+    });
+    console.log(id);
+    
   }
   
   return (
@@ -23,7 +48,15 @@ function App() {
       <NewUserForm />
       <ul>
         {data && data.users.map((user, index) => (
-          <li key={index}>{user.name}</li>
+          <li key={index}>
+            {user.name}
+            <button
+              type="button"
+              onClick={() => handleDelete(user.id)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
