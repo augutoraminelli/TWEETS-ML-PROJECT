53e45704619981669e04bd408fe5492dbeb2e906
Augusto <augusto.raminelli>
2022-06-05 17:28:04 -0300

    tests: 'implement first test'

--- frontend/src/tests/App.spec.tsx
+++ frontend/src/tests/App.spec.tsx
@@ -1,3 +1,9 @@
-test('should first', () => { 
-    expect(1 + 1).toBe(2);
- })
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('test1')).toBeInTheDocument();
+  });
+});
